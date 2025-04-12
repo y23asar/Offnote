47524ba0f39415f03d03e913d8cdae3135c49c3a
y23asar <yasarabdul007@gmail.com>
2025-04-12 14:57:21 +0530

    Deleted contents of sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,1 @@
-const cacheName = 'offline-files-v1';
-const assets = [
-  '/',
-  '/index.html',
-  '/style.css',
-  '/script.js',
-  '/idb.js',
-  'https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js'
-];
 
-self.addEventListener('install', event => {
-  event.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(res => res || fetch(event.request))
-  );
-});
