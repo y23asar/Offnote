c54e0e93760dd83d249d31b5c2a956c3be45157d
y23asar <yasarabdul007@gmail.com>
2025-04-12 13:29:59 +0530

    Add files via upload

---
+++ idb.js
@@ -0,0 +1,17 @@
+// Very minimal IndexedDB wrapper using idb
+const dbPromise = idb.openDB('fileDB', 1, {
+    upgrade(db) {
+      db.createObjectStore('files');
+    }
+  });
+  
+  async function saveFileToIDB(code, content) {
+    const db = await dbPromise;
+    await db.put('files', content, code);
+  }
+  
+  async function getFileFromIDB(code) {
+    const db = await dbPromise;
+    return db.get('files', code);
+  }
+  
